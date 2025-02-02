63eaea80412a6d60ac2367d8047e3491607dac9f
Jacob Shannon <jacobshnn@gmail.com>
2025-02-01 20:53:03 -0800

    Finish server-stat.sh

--- server-stats.sh
+++ server-stats.sh
@@ -11,14 +11,30 @@
 # Start the script below
 
 # Total CPU usage
+# The 8th field in the top command output 
+# shows the percentage of CPU that is not being used
 echo "Total CPU usage"
 top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8 "% CPU used"}'
+echo "-----------------------------------"
 
 # Total memory usage (Free vs Used including percentage)
 echo "Total memory usage"
-# Free shows the total amount of free and used physical and swap memory in the system, as well as the buffers used by the kernel, in MB.
+# Free shows the total amount of free and used physical and swap memory in the system, 
+# as well as the buffers used by the kernel, in MB.
 free -m | grep Mem | awk '{printf "Used: %dMB, Free: %dMB, Usage: %.2f%%\n", $3, $4, $3/$2*100}'
+echo "-----------------------------------"
 
 # Total disk usage (Free vs Used including percentage)
 echo "Total disk usage"
-df -h | grep /dev/sda1 | awk '{printf "Used: %s, Free: %s, Usage: %s\n", $3, $4, $5}'
+df -h | grep /dev/mapper/data-root | awk '{printf "Used: %s, Free: %s, Usage: %s\n", $3, $4, $5}'
+echo "-----------------------------------"
+
+# Top 5 processes by CPU usage
+echo "Top 5 processes by CPU usage"
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
+echo "-----------------------------------"
+
+# Top 5 processes by memory usage
+echo "Top 5 processes by memory usage"
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6
+echo "-----------------------------------"
